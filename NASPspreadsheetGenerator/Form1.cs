70c87c20f64b891eb15b8dab7263083de224e025
Graham Jones <rangerg@greatbigtable.com>
2023-05-22 21:03:20 -0500

    got the archer selection working, now just got to work on adding the school data.

--- NASPspreadsheetGenerator/Form1.cs
+++ NASPspreadsheetGenerator/Form1.cs
@@ -541,6 +541,43 @@ namespace NASPspreadsheetGenerator
                         changeItemShowing(temporarystorage[0]);
                     }
                 }
+                else
+                {
+                    var arrayOfAllKeys = archerData.Keys.ToArray();
+                    List<string> temporarystorage = new List<string>();
+                    foreach (string key in arrayOfAllKeys)
+                    {
+                        if (key.Contains(lstSecondary.SelectedItem.ToString()) && lstPrimary.SelectedItem.ToString().Contains(archerData[key][8]))
+                        {
+                            temporarystorage.Add(key);
+                        }
+                    }
+
+                    if (temporarystorage.Count > 1)
+                    {
+                        for (int i = 0; i < temporarystorage.Count; i++)
+                        {
+                            /*tournament name, school name, archer name("Graham Jones"), archer history ID,
+                            *grade, gender, state, country, end date, range type, division type (school only not gender),
+                            *rank (gender and divison, ex. High School Boys), score, 10s, 9s, 8s, 7s, Arrow 1, Arrow 2, Arrow 3,
+                            *Arrow 4, Arrow 5, Arrow 6, Arrow 7, Arrow 8, Arrow 9, Arrow 10, Arrow 11, Arrow 12, Arrow 13,
+                            *Arrow 14, Arrow 15, Arrow 16, Arrow 17, Arrow 18, Arrow 19, Arrow 20, Arrow 21, Arrow 22, Arrow 23,
+                            *Arrow 24, Arrow 25, Arrow 26, Arrow 27, Arrow 28, Arrow 29, Arrow 30
+                            */
+                            archerSelect.key.Add(temporarystorage[i]);
+                            archerSelect.names.Add(archerData[temporarystorage[i]][2]);
+                            archerSelect.gender.Add(archerData[temporarystorage[i]][5]);
+                            archerSelect.school.Add(archerData[temporarystorage[i]][1]);
+                            archerSelect.grade.Add(archerData[temporarystorage[i]][4]);
+
+                        }
+                        archerSelect.ShowDialog(this);
+                    }
+                    else
+                    {
+                        changeItemShowing(temporarystorage[0]);
+                    }
+                }
             }
         }
     }
